63752630eb38f393166951a5d71462e3ed5c6455
Mfar121 <127731220+Mfar121@users.noreply.github.com>
2023-04-10 13:41:22 +0100

    movie nerd

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 /* 
 👉 Write your kata here!
-*/
+// you are a tarantino buff and want to do a movie marathon. your friends want to know the exact runtime of his movies but refuse to watch more than 3 during the marathan. write code to calculate the runtime for any 3 combinations of his movies. please include the names of the movies chosen in the output.
+don't worry about exact runtimes; I will forgive you for your film transgressions.*/
+
+
+
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function calculateRuntime(movie1, movie2, movie3) {
+
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,33 @@
-//👉 Write your tests below here:
+// import the jest testing library
+import { test, expect } from '@jest/globals';
+// import the function to be tested
+import { calculateRuntime } from './main.js';
+// write array for the length of the movies
+const movies = [  { title: 'Reservoir Dogs', runtime: 99 },  { title: 'Pulp Fiction', runtime: 154 },  { title: 'Jackie Brown', runtime: 154 },  { title: 'Kill Bill Vol. 1', runtime: 111 },  { title: 'Kill Bill Vol. 2', runtime: 137 },  { title: 'Death Proof', runtime: 113 },  { title: 'Inglourious Basterds', runtime: 153 },  { title: 'Django Unchained', runtime: 165 },  { title: 'The Hateful Eight', runtime: 187 }];
+
+// write the test suite
+function calculateRuntime(movie1, movie2, movie3) {
+  const totalRuntime = [movie1, movie2, movie3].reduce((total, movie) => total + movie.runtime, 0);
+  return `${movie1.title}, ${movie2.title}, and ${movie3.title} have a total runtime of ${totalRuntime} minutes.`;
+}
+
+justJesting('calculateRuntime', () => {
+  test('should return the correct runtime for Reservoir Dogs, Kill Bill Vol. 1, and Death Proof', () => {
+    expect(calculateRuntime(movies[0], movies[3], movies[5])).toEqual('Reservoir Dogs, Kill Bill Vol. 1, and Death Proof have a total runtime of 323 minutes.');
+  });
+
+  test('should return the correct runtime for Pulp Fiction, Kill Bill Vol. 2, and Django Unchained', () => {
+    expect(calculateRuntime(movies[1], movies[4], movies[7])).toEqual('Pulp Fiction, Kill Bill Vol. 2, and Django Unchained have a total runtime of 456 minutes.');
+  });
+
+  test('should return the correct runtime for Jackie Brown, Inglourious Basterds, and The Hateful Eight', () => {
+    expect(calculateRuntime(movies[2], movies[6], movies[8])).toEqual('Jackie Brown, Inglourious Basterds, and The Hateful Eight have a total runtime of 494 minutes.');
+  });
+
+  test('should handle empty input correctly', () => {
+    expect(() => calculateRuntime()).toThrow();
+  });
+});
+
+
+
